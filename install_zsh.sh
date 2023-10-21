8856530cdefdc3ac5e1504befc2d1981637ddb4d
John Boccio <john.t.boccio@gmail.com>
2023-10-20 22:08:46 -0700

    Installing zsh plugins

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ZSHRC_PATH="$HOME/.zshrc"
 OH_MY_ZSH_PATH="$HOME/.oh-my-zsh"
@@ -17,5 +17,9 @@ else
 	exit 1
 fi
 
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
